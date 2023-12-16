6eca2aab7d38923707f287468dd2bde78e89e228
cesar <caesersoft@gmail.com>
2023-12-16 15:05:55 -0500

    2da peronalizacion index, estilos y java

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 //SIDEBAR TOGLLE
+const btnSignIn =document.getElementById("sign-in"),
+        btnSearch=document.getElementById("search"),
+        formSearch= document.querySelector(".search"),
+        formLogin=document.querySelector(".login"),
+        pass = document.getElementById("pass"),
+        icon = document.querySelector(".bx");
 
 var sidebarOpen = false;
 var sidebar = document.getElementById("sidebar");
@@ -17,3 +23,23 @@ function closeSidebar(){
         sidebarOpen = false;
     }
 }
+
+btnSignIn.addEventListener("click", e=> {
+    formSearch.classList.add("hide");
+    formLogin.classList.remove("hide");
+})
+
+btnSearch.addEventListener("click", e=> {
+    formLogin.classList.add("hide");
+    formSearch.classList.remove("hide");
+})
+
+icon.addEventListener("click", e=> {
+    if(pass.type === "password"){
+        pass.type = "text";
+        icon.innerHTML='visibility_off'
+    }else{
+        pass.type = "password";
+        icon.innerHTML='visibility'
+    }
+})
